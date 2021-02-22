2258fd6f82876de18b26e26b869f06a67dbfa9f9
jghm96 <jhernandezm@fi.uba.ar>
2021-02-22 18:49:48 -0300

    subi nuevo

--- main.cpp
+++ main.cpp
@@ -12,13 +12,6 @@ using namespace std;
 
 int main()
 {
-    Grafo g;
-    Matriz m;
-    LecturaCsvCasilleros l;
-    string csv = "mapa.csv";
-    l.lecturaArchivo(&m, &g, csv);
-    m.pasarAdyacencia(&g);
-    Personaje* p = new Tierra("mely", 1, 2);
-    cout << g.caminoMinimo(1, 1, 7, 4, p) << endl;
+
     return 0;
 }
